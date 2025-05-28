22945b9e681e64ce08395e0c3b1777dd969be782
Matheus Menezes <matheusmiranda98@live.com>
2025-05-28 15:02:41 -0300

    fixed no valid test

--- src/main/java/com/createlittlecontraptions/gametests/SimpleCLCTests.java
+++ src/main/java/com/createlittlecontraptions/gametests/SimpleCLCTests.java
@@ -5,11 +5,13 @@ import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.world.level.block.Blocks;
 import net.neoforged.neoforge.gametest.GameTestHolder;
+import net.neoforged.neoforge.gametest.PrefixGameTestTemplate;
 
+@PrefixGameTestTemplate(false)
 @GameTestHolder("createlittlecontraptions")
 public class SimpleCLCTests {    // Um teste básico para Create Little Contraptions
-    @GameTest
-    public void basicBlockPlacementTest(GameTestHelper helper) {
+    @GameTest(template = "test_templates/3x3x3_air")
+    public static void basicBlockPlacementTest(GameTestHelper helper) {
         // Posição relativa dentro da área de teste
         BlockPos pos = new BlockPos(0, 1, 0);
 
@@ -22,18 +24,4 @@ public class SimpleCLCTests {    // Um teste básico para Create Little Contrapt
         // Marca o teste como bem-sucedido
         helper.succeed();
     }
-
-    // Teste para verificar integração básica com Create
-    @GameTest(timeoutTicks = 100)
-    public void createIntegrationTest(GameTestHelper helper) {
-        // Posição relativa
-        BlockPos pos = new BlockPos(0, 1, 0);
-
-        helper.setBlock(pos, Blocks.REDSTONE_BLOCK);
-
-        // Espera alguns ticks e então executa verificação
-        helper.runAfterDelay(20, () -> {
-            helper.assertBlockPresent(Blocks.REDSTONE_BLOCK, pos);
-            helper.succeed();
-        });
-    }}
+}
